976a179b6a43f789d6c9ceabc491481e9b2b4c2e
wmancaniello <wmancaniello@gmail.com>
2024-04-13 13:22:07 +0200

    fix

--- js/script.js
+++ js/script.js
@@ -27,3 +27,5 @@ const images = [
     text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
   },
 ];
+
+
